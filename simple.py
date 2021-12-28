e71021b244d9d50b456b48d7df4120c6d66b61c9
Pavel Bures <pavel.bures@gmail.com>
2021-12-28 11:10:18 +0100

    Added comments, more examples

---
+++ simple.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import time
+
+import numpy as np
+from PIL import Image, ImageDraw
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+import yfinance as yf
+
+## https://pillow.readthedocs.io/en/latest/reference/ImageDraw.html#module-PIL.ImageDraw
+## https://luma-core.readthedocs.io/en/latest/intro.html
+## https://luma-led-matrix.readthedocs.io/en/latest/intro.html
+
+if __name__ == "__main__":
+    try:
+        # Vytvori objekt ktery umozni komunikovat s matici pomoci GPIO pinu. Koukni na
+        # https://www.etechnophiles.com/wp-content/uploads/2021/01/R-Pi-4-GPIO-Pinout.jpg
+        # My budeme pouzivat SPI0 (raspberry jich ma 2). 
+        serial = spi(port=0, device=0, gpio=noop())
+
+        # Vytvori objekt zarizeni - nasi LED matice. Musime mu dat spi objekt, pres ktery by mel mluvit.
+        # Dale mu rikame ze mame v zarizeni 4 matice, a ze jsou od vyrobce otocene o 90 stupnu.
+        device = max7219(serial, cascaded=4, block_orientation=-90, rotate=0, blocks_arranged_in_reverse_order=False)
+
+        # Vytvorime si objekt Obrazku na ktery budeme kreslit a pak jej zobrazime na matici.
+        img = Image.new('1', (32,8), color=0)
+
+        # Toto nam vytvori objekt Obrazku ktery umi ruzne prikazy pro kresleni
+        draw = ImageDraw.Draw(img)
+
+        # .point je prikaz (volani metody na objektu) aby nakreslil puntik na souradnici 3,2 a barvou 1.
+        # LED matrix ma jenom 2 barvy: 1 ... cervena, 0 ... vypnuto.
+        # Tady jsou dalsi metody pro kresleni:
+        # https://pillow.readthedocs.io/en/latest/reference/ImageDraw.html#module-PIL.ImageDraw
+        draw.point((3, 3), fill=1)
+        
+        # Toto nakresli obdelnik s rohy (1,1) a (30,6). 
+        draw.rectangle([(1,1), (30,6)], fill=None, outline=1, width=2)
+
+        # Toto prikaze obrazku at se dokonci kresleni. 
+        img.show()
+
+        # Toto rekne nasi matici aby tam zobrazila obrazek co jsme nakreslili
+        device.display(img)
+
+        # Pocka 10 vterin nez program skonci.
+        time.sleep(10)
+
+    except KeyboardInterrupt:
+        pass
